2b41e9a6698d4e51c8dc072cda05c3a5afa3ea00
aqsazahoor07 <aqsazahoor07@gmail.com>
2026-08-12 16:33:48 +0500

    Update index.php
    fixed error reporting

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
-<?php require __DIR__ . '/../public/index.php';
 
+
+<?php
+
+// Error reporting on
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Log errors
+ini_set('log_errors', 1);
+ini_set('error_log', '/tmp/php-error.log');
+
+require __DIR__ . '/../public/index.php';
